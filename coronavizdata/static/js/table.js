7ffe309fc9d5e1d2cce8406722525ed8b930ae7a
attila5287 <atiturkoz@hotmail.com>
2020-04-28 09:56:38 -0600

    table basic demo

--- coronavizdata/static/js/table.js
+++ coronavizdata/static/js/table.js
@@ -1,12 +1,12 @@
 var testdata = testdata; // from testdata.js
-console.log(testdata);
+// console.log(testdata);
 
 const $countrySelect = document.getElementById('opts'); // selectfield
 let defaultId = 213;  // turkey country code as initial value
 
-fetchTestData(testdata);
+fetchTestData();
 
-function fetchTestData(testdata){
+function fetchTestData(){
   selectOptionsUp(testdata);
   overallCountUp(testdata);
   chosenFiguresUp(testdata, defaultId);
@@ -15,11 +15,10 @@ function fetchTestData(testdata){
   d3.select('#opts')
   .on('change', function () {
     let chosenId = eval(d3.select(this).property('value'));
-      chosenFiguresUp(data, chosenId);
 
-    chosenFiguresUp(testdata, defaultId);
-    table4OnePlease(testdata, defaultId);
-    
+    chosenFiguresUp(testdata, chosenId);
+    chosenFiguresUp(testdata, chosenId);
+    table4OnePlease(testdata, chosenId);
   });
 }
 
@@ -56,71 +55,6 @@ function fetchLatestData() {
 
   });
 }
-
-function table4OnePlease(data, chosenId) {
-  console.log(' --- Data for Table --- ');
-  var countryList = [];
-  var populationList = [];
-  var deathsList = [];
-  var confirmedList = [];
-
-  var countryIds = [chosenId, 225, 137, 201];
-
-  countryIds.forEach(id => {
-    countryList.push(data.locations[id].country);
-    populationList.push(data.locations[id].country_population);
-    deathsList.push(data.locations[id].latest.deaths);
-    confirmedList.push(data.locations[id].latest.confirmed);
-  });
-
-  let chinaName = 'China';
-  let chinaPop = data.locations[49].country_population;
-  let chinaDeaths = 0;
-  let chinaConfirmed = 0;
-
-
-  let dictArray = [];
-  let dict = {
-    'name': '',
-    'population': 0,
-    'deaths': 0,
-    'confirmed': 0
-    };    
-  for (let i = 0; i < countryList.length; i++) {
-    
-    dict['name'] = countryList[i];
-    console.log(dict['name']);
-    dict['population'] = populationList[i];
-    dict['deaths']  = deathsList[i];
-    dict['confirmed'] = confirmedList[i];
-    dictArray.push(dict);
-    console.log(dict);
-  }
-  console.log(dictArray);
-
-
-  // sum all 33 provinces of China for death-confirmed total
-  for (let id = 49; id <= 81; id += 1) {
-    const province = data.locations[id];
-    chinaDeaths += province.latest.deaths;
-    chinaConfirmed += province.latest.confirmed;
-  }
-
-  countryList.push(chinaName);
-  deathsList.push(chinaDeaths);
-  populationList.push(chinaPop);
-  confirmedList.push(chinaConfirmed);
-
-d3.select("tbody")
-  .selectAll("tr")
-  .data(dictArray)
-  .enter()
-  .append("tr")
-  .html(function(d) {
-    return `<td>${d.name}</td><td>${d.population}</td><td>${d.deaths}</td><td>${d.confirmed}</td>`;
-  });
-}
-
 function chosenFiguresUp(data, chosenId) {
   const format = d3.format(',');
   const formatDecimal = d3.format('.4');
@@ -155,3 +89,88 @@ function overallCountUp(data) {
   d3.select('#overallConfirmed').text(format(data.latest.confirmed));
 }
 
+function table4OnePlease(data, chosenId) {
+  console.log(' --- Data for Table --- ');
+  var countryList = [];
+  var populationList = [];
+  var deathsList = [];
+  var confirmedList = [];
+
+  var countryIds = [chosenId, 225, 137, 201];
+
+  countryIds.forEach(id => {
+    countryList.push(data.locations[id].country);
+    populationList.push(data.locations[id].country_population);
+    deathsList.push(data.locations[id].latest.deaths);
+    confirmedList.push(data.locations[id].latest.confirmed);
+  });
+
+  let chinaName = 'China';
+  let chinaPop = data.locations[49].country_population;
+  let chinaDeaths = 0;
+  let chinaConfirmed = 0;
+
+    // sum all 33 provinces of China for death-confirmed total
+  for (let id = 49; id <= 81; id += 1) {
+    const province = data.locations[id];
+    chinaDeaths += province.latest.deaths;
+    chinaConfirmed += province.latest.confirmed;
+  }
+
+  countryList.push(chinaName);
+  deathsList.push(chinaDeaths);
+  populationList.push(chinaPop);
+  confirmedList.push(chinaConfirmed);
+
+
+  let dictArray = [];
+  
+  console.log('---init dictArray ---');
+  console.log(dictArray);
+  const format = d3.format(',');    
+  for (let i = 0; i < countryList.length; i++) {
+    console.log(' --- dict comprehend ---');
+    let dict = {
+      'name': '',
+      'population': 0,
+      'deaths': 0,
+      'confirmed': 0
+      };    
+    dict.name = countryList[i];
+    dict.population = format(populationList[i]);
+    dict.deaths  = format(deathsList[i]);
+    dict.confirmed = format(confirmedList[i]);
+    console.log(dict.name);
+    console.log(dict);
+    dictArray.push(dict);
+  }
+
+
+  console.log(' --- dictArray --- ');
+  console.log(dictArray);
+
+  let list_of_items = ['a','b','c','d'],
+    result = [];
+
+function myAttempt(arr, key){
+  arr.forEach(function(v){
+    let obj = {};
+    obj[key] = v;
+    result.push(obj);
+  });
+  console.log(' --- result ---');
+  console.log(result);
+}
+
+myAttempt(list_of_items, 'name');
+
+
+d3.select("tbody")
+  .selectAll("tr")
+  .data(dictArray)
+  .enter()
+  .append("tr")
+  .html(function(d) {
+    return `<td>${d.name}</td><td>${d.population}</td><td>${d.deaths}</td><td>${d.confirmed}</td>`;
+  });
+}
